683057265fa5ded11ac5c6a44b3a58182ff09822
F1sh1t <93012659+F1sh1t@users.noreply.github.com>
2021-11-22 16:21:34 +0300

    main.lua

--- main.lua
+++ main.lua
@@ -9,6 +9,29 @@ local FirstButton = FirstPage.AddButton("Rejoin", function()
 		local p = game:GetService("Players").LocalPlayer
 		ts:Teleport(game.PlaceId, p)
 end)
-local FirstToggle = FirstPage.AddToggle("NO-CD Knives", false, function(bug)
-print(bug)
+local FirstSlider = FirstPage.AddSlider("Walkspeed", {Min = 0, Max = 300, Def = 1}, function(t)
+local walkspeed = 32
+local speed = t + walkspeed*0.05
+local rocket = Instance.new("BodyPosition",game.Players.LocalPlayer.Character.Torso)
+local upvalue = 0
+rocket.maxForce = Vector3.new(12500,12500,12500)
+
+spawn(function()
+while true do
+wait()
+if game.Players.LocalPlayer.Character.Humanoid.FloorMaterial == Enum.Material.Air then
+rocket.Parent = game.Players.LocalPlayer.Character
+wait(0.5)
+else
+wait(0.5)
+rocket.Parent = game.Players.LocalPlayer.Character.Torso
+end
+end
+end)
+
+spawn(function()
+while true do
+wait()
+rocket.Position = Vector3.new(game.Players.LocalPlayer.Character.Torso.Position.X+game.Players.LocalPlayer.Character.Humanoid.MoveDirection.X*speed*5.4,game.Players.LocalPlayer.Character.Torso.Position.Y,game.Players.LocalPlayer.Character.Torso.Position.Z+game.Players.LocalPlayer.Character.Humanoid.MoveDirection.Z*speed*5.4)
+end
 end)
