1ad8d5b322f6bd966b5021ea296e1c8e12e0039d
F1sh1t <93012659+F1sh1t@users.noreply.github.com>
2021-11-22 18:07:28 +0300

    main.lua

--- main.lua
+++ main.lua
@@ -7,11 +7,13 @@ local FirstLabel = FirstPage.AddLabel("YBA cuz yes")
 local FirstButton = FirstPage.AddButton("Arrow Migration?", function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/F1sh1t/scripts/main/novouch"))()
 end)
+local FirstToggle = FirstPage.AddToggle("Stand Bypass %x Value", false, function(Valug)
+print(Valug)
+end)
 local FirstSlider = FirstPage.AddSlider("Arrow Stand Bypass", {Min = 0, Max = 100, Def = 1}, function(Valua)
 game:GetService("Players").LocalPlayer.Character.Humanoid.WalkSpeed = Valua
 end)
 local FirstDropdown = FirstPage.AddDropdown("Stand %x", {
-Value,
 "Star Platinum"
 "White Snake"
 "Anubis"
