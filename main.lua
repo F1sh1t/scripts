2e2b40fb4432c7ec084754e736a84a3fe448a127
F1sh1t <93012659+F1sh1t@users.noreply.github.com>
2021-11-22 15:57:02 +0300

    main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 local UILibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/F1sh1t/scripts/main/UILib"))()
 
 local MainUI = UILibrary.Load("EQux Hub")
-local FirstPage = MainUI.AddPage("YBA Subs")
+local FirstPage = MainUI.AddPage("Misc")
 
 local FirstLabel = FirstPage.AddLabel("Section 1")
 local FirstButton = FirstPage.AddButton("Rejoin [BUGGY]", function()
@@ -9,18 +9,6 @@ local FirstButton = FirstPage.AddButton("Rejoin [BUGGY]", function()
 		local p = game:GetService("Players").LocalPlayer
 		ts:Teleport(game.PlaceId, p)
 end)
-local FirstToggle = FirstPage.AddToggle("Hello", false, function(Value)
-print(Value)
-end)
-local FirstSlider = FirstPage.AddSlider("Hello", {Min = 0, Max = 255, Def = 50}, function(Value)
-print(Value)
-end)
-local FirstPicker = FirstPage.AddColourPicker("Hello", "white", function(Value)
-print(Value)
-end)
-local FirstDropdown = FirstPage.AddDropdown("Hello", {
-"Hello",
-"Goodbye"
-}, function(Value)
+local FirstToggle = FirstPage.AddToggle("NO-CD Knives", false, function(Value)
 print(Value)
 end)
