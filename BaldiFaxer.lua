3975b867b552c32a077da55bfd13c7acda182879
F1sh1t <93012659+F1sh1t@users.noreply.github.com>
2021-11-27 14:04:04 +0300

    BaldiFaxer.lua

--- BaldiFaxer.lua
+++ BaldiFaxer.lua
@@ -1,7 +1,7 @@
 local PlaceId = game.PlaceId
 
 if PlaceId == 1823470443 then
-	loadstring(game:HttpGet('loadstring(game:HttpGet("https://raw.githubusercontent.com/F1sh1t/scripts/main/BBF"))()'))()
+	loadstring(game:HttpGet('loadstring(game:HttpGet("https://raw.githubusercontent.com/F1sh1t/scripts/main/BBF.lua"))()'))()
 else
 	game.Players.LocalPlayer:kick("Game is not Baldi's Basics.")
 	wait(1)
