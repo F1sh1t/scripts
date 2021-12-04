8634054be301857b96b156d0b83ae5621fc5ccc5
F1sh1t <93012659+F1sh1t@users.noreply.github.com>
2021-12-04 15:46:25 +0300

    main.lua

--- main.lua
+++ main.lua
@@ -4,32 +4,16 @@ local MainUI = UILibrary.Load("EQux Hub")
 local FirstPage = MainUI.AddPage("Misc")
 local SecondPage = MainUI.AddPage("Scripts")
 
-local FirstLabel = FirstPage.AddLabel("YBA Crack Chances")
-local FirstToggle = FirstPage.AddToggle("Stand Bypass %xbn", false, function(Valug)
-print(Valug)
-end)
-local FirstSlider = FirstPage.AddSlider("Arrow Stand Bypass v2.0 (Rec. 85)", {Min = 0, Max = 125, Def = 1}, function(Valua)
-game:GetService("Players").LocalPlayer.Character.Humanoid.WalkSpeed = Valua
-end)
-local FirstDropdown = FirstPage.AddDropdown("Stand Req. v2.0", {
-"The World",
-"Star Platinum",
-"White Snake",
-"Anubis",
-"King Crimson (FIXED)",
-"Six Pistols",
-"Magicians Red"
-}, function(Value)
-print("Stand over chances selected by")
-end)
-local FirstDropdown = FirstPage.AddDropdown("True Skin", {
-"The World: Girl World",
-"Star Platinum: Blur",
-"Anubis: Giant Swords",
-"King Crimson: Oreo",
-"Magicians Red: Over Heaven"
-}, function(Value)
-print("Stando skins activated!")
+local FirstLabel = FirstPage.AddLabel("YBA so sad but ye")
+local FirstToggle = FirstPage.AddToggle("Arigato. Arcade. Goodbye...", false, function(Valug)
+local module = require(game.ReplicatedStorage.Modules.FunctionLibrary)
+module.ItemMachine = {
+{
+Name = "Lucky Arrow",
+Percentage = 30,
+Color = Color3.fromRGB(255, 255, 0)
+}
+}
 end)
 local FirstLabel = SecondPage.AddLabel("Developer: Fishit#0879")
 local FirstButton = SecondPage.AddButton("Inject Script", function()
