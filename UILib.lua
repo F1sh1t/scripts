045231536c40a5bb4cbcd6c8cbf7d5e3f79941ac
F1sh1t <93012659+F1sh1t@users.noreply.github.com>
2021-12-06 00:12:34 +0300

    UILib.lua

--- UILib.lua
+++ UILib.lua
@@ -197,7 +197,7 @@ function UILibrary.Load(GUITitle)
 	MainFrame.Name = "MainFrame"
 	MainFrame.Size = UDim2.new(1,-50,1,-30)
 	MainFrame.Position = UDim2.new(0,25,0,15)
-	MainFrame.ImageColor3 = Color3.fromRGB(252, 140, 3)
+	MainFrame.ImageColor3 = Color3.fromRGB(136, 0, 204)
 	MainFrame.Parent = ContainerFrame
 	
 	local MenuBar, DisplayFrame, TitleBar
