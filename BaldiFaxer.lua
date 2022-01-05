e37e00c78edbc8c263533539545a3cc36ef8de12
F1sh1t <93012659+F1sh1t@users.noreply.github.com>
2022-01-06 01:03:44 +0300

    Delete BaldiFaxer.lua

--- BaldiFaxer.lua
+++
@@ -1,9 +0,0 @@
-local PlaceId = game.PlaceId
-
-if PlaceId == 1823470443 then
-	loadstring(game:HttpGet('loadstring(game:HttpGet("https://raw.githubusercontent.com/F1sh1t/scripts/main/BBF.lua"))()'))()
-else
-	game.Players.LocalPlayer:kick("Game is not Baldi's Basics.")
-	wait(1)
-	game:Shutdown()
-end
